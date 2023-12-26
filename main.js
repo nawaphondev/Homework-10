49a9cbf8142cd4dc384a1bd02005e06334e51bda
nawaphondev <nawaphonphophom55@gmail.com>
2023-12-26 11:22:21 +0700

    Update Tenth Commit

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const root = ReactDOM.createRoot(document.querySelector('#root'));
 const CounterComponent = () => {
     return (
         <div className="counter">
-            <button class="btn-inc">+</button>
-            <h2>{0}</h2>
             <button class="btn-dec">-</button>
-            <button class="btn-clr">0</button>
+            <h2>{0}</h2>
+            <button class="btn-inc">+</button>
+            <button class="btn-clr">C</button>
             <button class="btn-rem">X</button>
         </div>
     );
